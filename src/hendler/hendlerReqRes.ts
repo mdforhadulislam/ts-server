af99e0d3be3f10565d0d638633e262376babca4a
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-04-11 22:09:42 +0600

    add file:

--- src/hendler/hendlerReqRes.ts
+++ src/hendler/hendlerReqRes.ts
@@ -1,7 +1,16 @@
 const hendler: any = {};
 
 hendler.hendlerReqRes = (req: any, res: any) => {
-  res.end("Hello world");
+  if (req.url === "/") {
+    res.write("Home Page");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("About Page");
+    res.end();
+  } else {
+    res.write("404 Page");
+    res.end();
+  }
 };
 
 export default hendler;
