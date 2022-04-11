f28afc2dd76d74961b2655ff019bbf00ac76bd2d
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-04-11 22:13:28 +0600

    upadte file

--- dist/hendler/hendlerReqRes.js
+++ dist/hendler/hendlerReqRes.js
@@ -2,6 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var hendler = {};
 hendler.hendlerReqRes = function (req, res) {
-    res.end("Hello world");
+    if (req.url === "/") {
+        res.write("Home Page");
+        res.end();
+    }
+    else if (req.url === "/about") {
+        res.write("About Page");
+        res.end();
+    }
+    else {
+        res.write("404 Page");
+        res.end();
+    }
 };
 exports.default = hendler;
